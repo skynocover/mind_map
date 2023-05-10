e20c68063772d53b082c98cd525cec2ba7093dfc
EricWu <skynocover@gmail.com>
2023-05-10 17:31:26 +0800

    refactor: 修改firebase config

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,15 +1,14 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
-import { getFirestore, Firestore } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBCzxE4SbUwokxVt-Pi5IJE_JJPrYmYPbk',
-  authDomain: 'mindmap-c7d8f.firebaseapp.com',
-  projectId: 'mindmap-c7d8f',
-  storageBucket: 'mindmap-c7d8f.appspot.com',
-  messagingSenderId: '510149345910',
-  appId: '1:510149345910:web:4269f27a4d062681b45557',
-  measurementId: 'G-D31613VKNJ',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
